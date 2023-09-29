e92b07810d1ae28fe47cc6cefccd6522bae6083b
Bex1228 <138470472+Bex1228@users.noreply.github.com>
2023-09-29 21:26:28 +0600

    FirstFragment.kt

--- app/src/main/java/com/example/hw_3_6_kotlin/FirstFragment.kt
+++ app/src/main/java/com/example/hw_3_6_kotlin/FirstFragment.kt
@@ -13,27 +13,27 @@ import com.example.hw_3_6_kotlin.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
 
-    private lateinit var binding: FragmentFirstBinding  // Используйте ваш класс биндинга для фрагмента
+    private lateinit var binding: FragmentFirstBinding 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentFirstBinding.inflate(inflater, container, false)  // Используйте метод inflate для биндинга
+        binding = FragmentFirstBinding.inflate(inflater, container, false)  
 
 
         val itemList = listOf("Алладин","Симба","Маккуин","Бэмби","Чудовище","Золушка","Чип","Дэйл","Лило","Стичь","Микки Маус","Винни Пух",
             "Балу","Маугли","Пумба","Белоснежка","Тарзан","Мулан","Ахилес","Шрек")
 
-        // Получите имя, которое вы хотите передать в адаптер
+        
         val name = ""
 
-        // Создайте адаптер, передавая класс биндинга фрагмента
+        
         val adapter = PersonageAdapter(requireContext(), name, itemList)
 
         binding.rvContainer.adapter = adapter
         binding.rvContainer.layoutManager = LinearLayoutManager(requireContext())
 
-        return binding.root  // Верните корневой элемент биндинга в качестве view
+        return binding.root  
     }
 }
